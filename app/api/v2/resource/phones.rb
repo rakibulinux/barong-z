7b3764c6c5b81e53376cb52f0ea2f8eb9106aaa0
Hữu Hà <business@zsmart.tech>
2022-05-02 00:31:12 +0000

    Fix create phone

--- app/api/v2/resource/phones.rb
+++ app/api/v2/resource/phones.rb
@@ -76,7 +76,8 @@ module API::V2
               phone.number = phone_number
               phone.save!
             else
-              phone = Phone.create(user: current_user, code_id: code_id, number: phone_number)
+              phone = Phone.create(user: current_user, code: code, number: phone_number)
+              phone.code_id = code_id
               phone.save!
             end
 
